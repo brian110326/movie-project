a009872f30168d7bd6171238fa4a9f229e434bef
brian <brian110326@gmail.com>
2025-08-06 14:44:41 +0900

    movie-project

--- seat-service/src/main/java/com/example/seatservice/messagequeue/KafkaConsumerConfig.java
+++ seat-service/src/main/java/com/example/seatservice/messagequeue/KafkaConsumerConfig.java
@@ -28,7 +28,7 @@ public class KafkaConsumerConfig {
         deserializer.setUseTypeMapperForKey(true);
 
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.19.0.101:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroup");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
